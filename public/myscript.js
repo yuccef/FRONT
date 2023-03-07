85e5798f6088e2ad29a88a6203c114991ca07e6e
youssef ABBADI <youssefabbadipro@gmail.com>
2023-03-07 18:22:24 +0100

    Edits in list and myscrirpt code

--- public/myscript.js
+++ public/myscript.js
@@ -1,4 +1,4 @@
-const custormers = [
+const customers = [
 {"id":1,"email":"isidro_von@hotmail.com","first":"Torrey","last":"Veum","company":"Google","created_at":"2014-12-25T04:06:27.981Z","country":"Switzerland"},
 {"id":2,"email":"frederique19@gmail.com","first":"Micah","last":"Sanford","company":"Google","created_at":"2014-07-03T16:08:17.044Z","country":"Democratic People's Republic of Korea"},
 {"id":3,"email":"fredy54@gmail.com","first":"Hollis","last":"Swift","company":"Microsoft","created_at":"2014-08-18T06:15:16.731Z","country":"Tunisia"},
@@ -31,6 +31,6 @@ const custormers = [
 addEventListener("DOMContentLoaded",
 function(){
     log.console("le fichier est chargé");
-    $("#customers").loadtemplate($("#tpnCustomers"),customers);
+    $("#customers").loadTemplate($("#tpnCustomer"),customers);
 
 });

aeda0085c65bb87fe2df0e58a6e47fad99940162
youssef ABBADI <youssefabbadipro@gmail.com>
2023-03-07 15:03:21 +0100

    Edits

--- index.js
+++ index.js
@@ -8,11 +8,6 @@ const port = 3000;
 function main(){
 
     app.use(express.static("public"));
-
-    app.get('/page1', (req, res) => {
-        res.sendFile(__dirname + '/public/list.html');
-      });
-      
     
     app.listen(port , () => {
         console.log(`Example app listening on port   ${port}`);
--- index1.js
+++ index1.js
@@ -5,6 +5,9 @@ const port = 3000;
 
 
 function main(){
+
+    app.use(express.static("public"));
+
     app.get("/" , (req,res)=>{
         const currentDate = new Date();
         console.log("page callse at " + currentDate);
